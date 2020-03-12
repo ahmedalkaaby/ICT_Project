d52dacc64adef94f9799af40c9bf9eafcd197723
ahmed atiyah <ahmed.13.alkaaby@gmail.com>
2020-03-12 21:56:35 +0300

    deleting unnecessary files

--- GQLConfig.js
+++
@@ -1,9 +0,0 @@
-//urls credentials
-module.exports = {
-  graphQLEndpoint:
-    'http://ec2-54-90-219-131.compute-1.amazonaws.com:8080/wordpress/graphql',
-  headers: {},
-  method: 'POST',
-  credentials: 'same-origin',
-  siteUrl: '',
-};
